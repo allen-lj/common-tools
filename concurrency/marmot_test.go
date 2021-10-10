c048aed5ddfc2c1378f92281af7a3a6a386654a4
lj <allen@ljdeMacBook-Pro.local>
2021-10-10 21:14:19 +0800

    add test for marmot

---
+++ concurrency/marmot_test.go
@@ -0,0 +1,18 @@
+package concurrency
+
+import "testing"
+
+func TestNewMarmot(t *testing.T) {
+	m := NewMarmot(3, 1)
+	m.AddProcessor(&MockProcessor{})
+	go m.StartWork()
+	m.WaitForClose()
+	m.CloseWorkQueue()
+}
+
+type MockProcessor struct {
+}
+
+func (m *MockProcessor) PreProcess()   {}
+func (m *MockProcessor) DoProcess()    {}
+func (m *MockProcessor) AfterProcess() {}
